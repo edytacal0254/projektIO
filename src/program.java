63348211b9d3b098e4d27d0d6d56180f2ee1f74e
edytacal0254 <44200308+edytacal0254@users.noreply.github.com>
2020-11-28 20:00:44 +0100

    problem z hog, uszkadza stos

--- src/program.java
+++ src/program.java
@@ -1,12 +1,11 @@
 import org.opencv.core.*;
 import org.opencv.highgui.HighGui;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.HOGDescriptor;
 import org.opencv.video.BackgroundSubtractor;
 import org.opencv.video.Video;
 import org.opencv.videoio.VideoCapture;
-import org.opencv.videoio.Videoio;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -43,8 +42,10 @@ public class program {
             System.exit(0);
         }
 
-            Mat frame1 = new Mat();
+
+        Mat frame1 = new Mat();
         Mat frame2 = new Mat();
+
 
             //capture.read(frame1);   //poprawić dobrze by było gdyby to było tło
         capture.read(frame2); //poprawić dobrze by było gdyby to było tło
@@ -74,23 +75,30 @@ public class program {
                 //Imgproc.dilate(thres,dilated,Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,new Size(3,3)),new Point(-1,-1),3);
                 //Imgproc.findContours(thres,3,2);
                 //HighGui.imshow("FG Mask", thres);
+                //obejdzie się bez nich
 
             List<MatOfPoint> contours = new ArrayList<>();
             Mat hier = new Mat();
             Imgproc.findContours(diff,contours,hier,Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
 
-            Rect r = null;
+
+
+            ArrayList<Mat> potentialHumans = new ArrayList<>();
+
+            Rect r;
             ArrayList<Rect> rect_array = new ArrayList<>();
-            double maxArea = 100;
-            int maxAreaIdx = -1;
+            //double maxArea = 5000; //100*50
+            //int maxAreaIdx = -1;
             for (int idx = 0; idx < contours.size(); idx++) {
-                Mat contour = contours.get(idx);
-                double contourarea = Imgproc.contourArea(contour);
-                if (contourarea > maxArea) {
-                maxAreaIdx = idx;
-                r = Imgproc.boundingRect(contours.get(maxAreaIdx));
-                rect_array.add(r);
-                }
+                //Mat contour = contours.get(idx);
+                //double contourarea = Imgproc.contourArea(contour);
+                //if (contourarea >= maxArea) {
+                //maxAreaIdx = idx;
+                r = Imgproc.boundingRect(contours.get(idx));
+                    if (r.width>=50 && r.height>=100) {
+                        rect_array.add(r);
+                    }
+                //}
             }
             if (rect_array.size() > 0) {
                 Iterator<Rect> it2 = rect_array.iterator();
@@ -98,18 +106,72 @@ public class program {
                     Rect obj = it2.next();
                     Imgproc.rectangle(frame2, obj.br(), obj.tl(),
                             new Scalar(0, 255, 0), 1);
+
+                    Mat temp1 = new Mat(frame2,obj);
+
+                    potentialHumans.add(temp1.clone());
+
+                    //hog.detectMultiScale(temp,found,foundWeights);
+
+                    /*ArrayList<Rect> found_array = new ArrayList<>();
+                    found_array.addAll(Arrays.asList(found.toArray()));
+                    Iterator<Rect> it3 = found_array.iterator();
+                    while (it3.hasNext()){
+                        Rect obj3 = it3.next();
+                        Imgproc.rectangle(frame2,obj3.br(),obj3.tl(),new Scalar(255,0,0),1);
+                    }*/
+
+
                 }
 
             }
+            MatOfRect found = new MatOfRect();
+            MatOfDouble foundWeights = new MatOfDouble();
+
+            //próba nr1
+            /*
+            HOGDescriptor hog = new HOGDescriptor();
+            hog.setSVMDetector(HOGDescriptor.getDefaultPeopleDetector());
+            Iterator<Mat> it3 = potentialHumans.iterator();
+            while (it3.hasNext()){
+                Mat temp = it3.next();
+                hog.detectMultiScale(temp,found,foundWeights);
+                //uszkadza  stos.....
+
+            }*/
+
+
+            //próba nr2 (z wątkami)
+            /*
+            for(Mat t : potentialHumans){
+
+                //HOGDescriptor hog = new HOGDescriptor();
+                //hog._winSize = Size(48, 96);
+                //HOGDescriptor hog = new HOGDescriptor(new Size(48,96),new Size(12,12),new Size(6,6),new Size(6,6),9);
+                //hog.setSVMDetector(HOGDescriptor.getDaimlerPeopleDetector());
+                // powinien być daimler ale nie da się go załadować <?>
+                HOGDescriptor hog = new HOGDescriptor();
+                hog.setSVMDetector(HOGDescriptor.getDefaultPeopleDetector());
+
+                //HighGui.imshow("framhjhhhe2",t);
+                Detection det = new Detection(t,hog,found,foundWeights);
+                Thread thread = new Thread(det);
+                //hog.detectMultiScale(t,found,foundWeights);
+                //uszkadza stos.....
+                thread.start();
+            }
+            */
+
+
 
             HighGui.imshow("frame2",frame2);
 
             capture.read(frame1);
-            HighGui.imshow("framdde2",frame1);
+            HighGui.imshow("orginal",frame1);
 
-            HighGui.imshow("dilated", diff);
-            // get the input from the keyboard
-            // (nie mam pojęcia po co to ale w każdym tutorialu jest)
+            HighGui.imshow("motion", diff);
+
+            // tylko do HighGui
             int keyboard = HighGui.waitKey(30);
             if (keyboard == 'q' || keyboard == 27) {
                 break;
