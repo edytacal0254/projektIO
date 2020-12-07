545faf17510dcd76e328ec575dad264431b4c645
edytacal0254 <44200308+edytacal0254@users.noreply.github.com>
2020-12-07 21:49:16 +0100

    zostały parametry

--- src/program.java
+++ src/program.java
@@ -9,9 +9,9 @@ import org.opencv.videoio.VideoWriter;
 import org.opencv.videoio.Videoio;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -20,26 +20,26 @@ import java.util.List;
 public class program {
     static{ System.loadLibrary(Core.NATIVE_LIBRARY_NAME); }
 
-    public static ArrayList<double[]> colorRGB=new ArrayList<>();
-    public static ArrayList<String> colorNames=new ArrayList<>();
-    public static String path;
-    public static Size frameSize;
-    public static String tempVideoPath;
-    public static String tempVideoFormat;
-    public static File logs;
-    public static PrintWriter logsWriter;
+    private static final ArrayList<double[]> colorRGB=new ArrayList<>();
+    private static final ArrayList<String> colorNames=new ArrayList<>();
+    private static String path;
+    private static Size frameSize;
+    private static String tempVideoPath;
+    private static String tempVideoFormat;
+    private static File logs;
+    private static PrintWriter logsWriter;
 
 
     public static void setPath(String path) throws IOException {
         String tempPath = path.substring(0,path.length()-4);
         String format = path.substring(path.length()-4);
-        String logsPath = (new StringBuilder().append(tempPath).append("_logs.txt")).toString();
+        String logsPath = tempPath + "_logs.txt";
 
         program.path = path;
-        program.tempVideoPath = (new StringBuilder().append(tempPath).append("_tempOutput").append(format)).toString();
+        program.tempVideoPath = tempPath + "_tempOutput" + format;
         program.tempVideoFormat = format;
         program.logs = new File(logsPath);
-        program.logsWriter = new PrintWriter(logsPath,"UTF-8");
+        program.logsWriter = new PrintWriter(logsPath, StandardCharsets.UTF_8);
     }
 
     public static String getPath() {
@@ -67,7 +67,7 @@ public class program {
         if (!tempVideoPath.isEmpty()){
             //nie jestem pewna czy tu musimy sprawdzać czy w gui automatycznie dorzuci format
             if (!path.endsWith(".mp4") || !path.endsWith(".avi")) {
-                path = (new StringBuilder().append(path).append(getTempVideoFormat())).toString();
+                path = path + getTempVideoFormat();
             }
 
             File tempVideo = new File(getTempVideoPath());
@@ -89,7 +89,7 @@ public class program {
         int seconds = (int) (((frameMs-miliseconds)/1000)%60);
         int minutes = (int) ((((frameMs-miliseconds)/1000)-seconds)/60);
         String delim =":";
-        String time = (new StringBuilder().append(minutes).append(delim).append(seconds).append(delim).append((int)miliseconds)).toString();
+        String time = minutes + delim + seconds + delim + (int) miliseconds;
         return time;
     }
 
@@ -132,6 +132,7 @@ public class program {
         float green=0;
         float blue=0;
 
+        //Wyliczamy średni kolor
         for (int i = 0; i < img.rows(); i++) {
             for (int j = 0; j < img.cols(); j++) {
                 double[] rgb = img.get(i,j);
@@ -141,6 +142,7 @@ public class program {
             }
         }
 
+        //Sprawdzamy który kolor z listy jest najbliższy do wyliczonego koloru
         double distance=442; //(0,0,0)--(255,255,255) ok. 441.6
         int colIdx=0;
         for (int j=0; j <colorRGB.size();j++){
@@ -160,9 +162,9 @@ public class program {
 
     public static void processVideo() throws InterruptedException, IOException {
         System.loadLibrary("opencv_videoio_ffmpeg450_64");
+
         BackgroundSubtractor backSub = Video.createBackgroundSubtractorKNN();
-        //na podstawie przykładowego filmiku
-        //trudno stwierdzić,który lepiej sobie radzi ten czy MOG2
+
         String path = getPath();
         VideoCapture capture = new VideoCapture(path);
         if (!capture.isOpened()) {
@@ -172,63 +174,37 @@ public class program {
             return;
         }
 
-
-        Mat frameOrginal;
         Mat frameForModifing = new Mat();
 
         HOGDescriptor hog = new HOGDescriptor(new Size(48,96), new Size(16,16), new Size(8,8), new Size(8,8), 9);
         hog.setSVMDetector(HOGDescriptor.getDaimlerPeopleDetector());
 
         capture.read(frameForModifing);
-        frameOrginal=frameForModifing.clone();
         setFrameSize(frameForModifing);
 
-        //ok
-        //System.out.println("format: "+getTempVideoFormat());
-        //System.out.println("file path: "+getPath());
-        //System.out.println("temp file path: "+getTempVideoPath());
-
-        //ustalamy rozszerzenie i inicjujemy VideoWriter
+        //Ustalamy rozszerzenie i inicjujemy VideoWriter
         int fourcc;
         if (getTempVideoFormat().equals(".avi")) {
             fourcc = VideoWriter.fourcc('M', 'J', 'P', 'G' );
         }else {
             fourcc= VideoWriter.fourcc('m','p','4','v');
         }
-
         VideoWriter videoWriter= new VideoWriter(getTempVideoPath(),fourcc,capture.get(Videoio.CAP_PROP_FPS),getFrameSize());
 
-        //właściwe przetwarzanie wideos
-
         String colorsInFrame="";
-        Integer humansInFrame=0;
+        int humansInFrame=0;
+
+        //właściwe przetwarzanie wideos
         while (capture.isOpened()){
             capture.read(frameForModifing);
             if (frameForModifing.empty()) {
                 break;
             }
-            Mat diff = new Mat(); //do wykrycia ruchu
-                //Mat gray = new Mat();
-                //Mat blur = new Mat();
-                //Mat thres = new Mat();
-                //Mat dilated = new Mat();
-                //Core.absdiff(frameOrginal,frameForModifing,diff);
-
+            Mat diff = new Mat();
             backSub.apply(frameForModifing,diff);
             Imgproc.GaussianBlur(diff,diff,new Size(5,5),0);
             Imgproc.threshold(diff,diff,20,255,Imgproc.THRESH_BINARY);
-            //chyba wystarczy bez tego,
-            //Imgproc.dilate(diff,diff,Imgproc.getStructuringElement(Imgproc.CV_SHAPE_ELLIPSE,new Size(3,3)),new Point(-1,-1),3);
-
             Imgproc.morphologyEx(diff,diff,Imgproc.MORPH_OPEN,Imgproc.getStructuringElement(Imgproc.CV_SHAPE_ELLIPSE,new Size(3,3)));
-
-                //Imgproc.cvtColor(diff,gray,Imgproc.COLOR_BGR2GRAY);
-                //Imgproc.GaussianBlur(gray,blur,new Size(5,5),0);
-                //Imgproc.threshold(blur,thres,20,255,Imgproc.THRESH_BINARY);
-                //Imgproc.dilate(thres,dilated,Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,new Size(3,3)),new Point(-1,-1),3);
-                //Imgproc.findContours(thres,3,2);
-                //HighGui.imshow("FG Mask", thres);
-                //obejdzie się bez nich
 
             List<MatOfPoint> contours = new ArrayList<>();
             Mat hier = new Mat();
@@ -237,18 +213,11 @@ public class program {
             //zbieramy Rect z wykrytym ruchem
             Rect motionRect;
             ArrayList<Rect> motionRectArray = new ArrayList<>();
-            //double maxArea = 5000; //100*50
-            //int maxAreaIdx = -1;
             for (int idx = 0; idx < contours.size(); idx++) {
-                //Mat contour = contours.get(idx);
-                //double contourarea = Imgproc.contourArea(contour);
-                //if (contourarea >= maxArea) {
-                //maxAreaIdx = idx;
                 motionRect = Imgproc.boundingRect(contours.get(idx));
                     if (motionRect.width>=50 && motionRect.height>=100 && !(motionRect.size().equals(getFrameSize()))) {
                         motionRectArray.add(motionRect);
                     }
-                //}
             }
 
 
@@ -261,13 +230,7 @@ public class program {
                 while (it2.hasNext()) {
                     Rect rectForDetecting = it2.next();
 
-                    //do testów, zaznacza wykryty ruch
-                    //Imgproc.rectangle(frameForModifing, rectForDetecting.br(), rectForDetecting.tl(), new Scalar(0, 255, 0), 1);
-
                     Mat frameForDetector = new Mat(frameForModifing,rectForDetecting);
-
-                    //do testów
-                    //HighGui.imshow("rero",frameForDetector);
 
                     //Todo dopasować parametry [przy domyślych jest nadgorliwy]
                     hog.detectMultiScale(frameForDetector,found,foundWeights);//,0,new Size(8,8),new Size(4,4),1.05,1,true);
@@ -277,69 +240,49 @@ public class program {
                     for (int i=0;i<humans.length;i++){
                         Rect rectForOrginalImage = new Rect(rectForDetecting.x + humans[i].x, rectForDetecting.y + humans[i].y, humans[i].width, humans[i].height);
                         Imgproc.rectangle(frameForModifing, rectForOrginalImage, new Scalar(0, 0, 255), 1);
-
-                        videoWriter.write(frameForModifing);
-
                         Mat forColor = new Mat(frameForModifing,rectForOrginalImage);
                         String color = getMeanRGBColor(forColor.clone());
-
                         colorsInFrame += color + ", ";
-                        //do testów, trzeba poprawić parametry, zaznacza za duży obszar
-                        //if (!color.equals("grey")){
-                        //  System.out.println(color);
-                        //}
                     }
+
                     humansInFrame+=humans.length;
 
                 }
 
             }
 
-            //uzupełniamy log dla konkretnej ramki
+            //uzupełnianie plików wynikowych dla konkretnej ramki
+
+            videoWriter.write(frameForModifing);
+
             if (!colorsInFrame.equals("")) {
-                colorsInFrame.substring(0, colorsInFrame.length() - 2);
+                colorsInFrame = colorsInFrame.substring(0, colorsInFrame.length() - 2);
             }
+
             String delim = " - ";
-            String log = (new StringBuilder().append(calcTimeStamp(capture.get(Videoio.CAP_PROP_POS_MSEC))).append(delim)
-                    .append(humansInFrame).append(delim).append(colorsInFrame)).toString();
+            String log = calcTimeStamp(capture.get(Videoio.CAP_PROP_POS_MSEC)) + delim +
+                    humansInFrame + delim + colorsInFrame;
             logsWriter.println(log);
             logsWriter.flush();
             colorsInFrame="";
             humansInFrame=0;
 
-
-            //* do testów
-            //HighGui.imshow("frameForModifing",frameForModifing);
-            //capture.read(frameOrginal);
-            //HighGui.imshow("original",frameOrginal);
-            //HighGui.imshow("motion", diff);
-
-            ////do HighGui
-            //int keyboard = HighGui.waitKey(30);
-            //if (keyboard == 'q' || keyboard == 27) {
-            //    break;
-            //}
-            //Thread.sleep(100);
-            //*
-
         }
-        //do testów
+
         capture.release();
         videoWriter.release();
 
-        //HighGui.waitKey();
-
-
     }
+
+    //do testów bez GUI
     public static void main(String[] args) throws InterruptedException, IOException {
-        String input = "C:\\Users\\edyta\\IdeaProjects\\projektIO\\src\\grupaB1.mp4";
-        //String input = "grupaB1.mp4";
-        //processVideo("grupaB1.mp4");
+        //ścieżka do pliku musi być absolutna
+        String input = "C:\\Users\\edyta\\IdeaProjects\\projektIO\\src\\grupaB1.avi";
         setColorArray();
         setPath(input);
         processVideo();
-        //deleteTempVideo();
-        //nie chce wczytać pliku ze ścieżki względnej <?>
+        saveVideoAs("C:\\Users\\edyta\\IdeaProjects\\projektIO\\src\\testutest");
+        deleteTempVideo();
         System.exit(0);
     }
 
